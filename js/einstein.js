0dcb5995eab4a606f879c755734ccbeda0c5bae5
osimjon <osimjon90@yandex.ru>
2023-02-18 01:12:00 +0300

    Три задания выполнены

--- js/einstein.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-{
-  const myWeight = +prompt`Запишите  массу вашего тела`,
-    speedLight = 3e8,
-    energy = myWeight * Math.pow(speedLight, 2);
-  console.log(`Энергия вашего тело ${energy} Дж`);
-}
--- js/index.js
+++ js/index.js
@@ -1,38 +1,10 @@
 "use stirict";
 {
-  /*Тема 1*/
-  /*Обязательный блок  */
-  const product = "Imac";
-  console.log(product);
-  const quantityProduct = 12;
-  console.log(quantityProduct);
-  const categoryProduct = "laptop";
-  console.log(categoryProduct);
-  const priceProduct = 12500;
-  console.log(priceProduct);
-  /* Не обязательный блок  */
-  const result = quantityProduct * priceProduct;
-  console.log("Общая стоимость товаров " + result + "₽");
+  const answer = prompt("Введите ваше число: ", "Введите число: 0-9 ");
+  if (isNaN(answer)) {
+    alert("Вы ввели некорректные данные, попробуйте снова!");
+  } else {
+    alert("Принято!");
+  }
+  alert(`Вы ввели ${answer} и это правильное значение !`);
 }
-/*Тема 2*/
-{
-  const product = "Яндекс TV";
-  const count = 8;
-  const categoty = "TV";
-  const price = 15000;
-  console.log(
-    `На складе ${count} шт ${product} на общую стоитмость: ${count * price}₽ `
-  );
-}
-/*Тема 3*/
-// {
-//   const product = prompt(`Введите наименование товара `);
-//   const count = +prompt(`Введите количество товара `);
-//   const category = prompt(`Название категории `);
-//   const price = +prompt(`Введите цену товара `);
-//   console.log(
-//     `На складе у нас ${count} шт ${product} что входит в категорию ${category} на сумму ${
-//       price * count
-//     } ₽ `
-//   );
-// }
